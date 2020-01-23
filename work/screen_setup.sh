d64a4ba8fb72e550961c46af7fecb8f5426a819c
alrasch <arasch855@gmail.com>
2020-01-23 15:29:04 +0100

    Add script for applying screen setup with background image.

--- work/screen_setup.sh
+++ work/screen_setup.sh
@@ -3,4 +3,8 @@ xrandr --output DP1 --primary --mode 1920x1080 --pos 1920x0 --rotate normal
 
 xrandr --output DP2 --mode 1920x1080 --pos 3850x0 --rotate normal --output DP3 --off 
 
-xrandr --output HDMI1 --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI2 --off 
+xrandr --output HDMI1 --mode 1920x1080 --pos 0x0 --rotate left  --output HDMI2 --off 
+
+# Set background image
+
+ls -l $HOME/Dev/wallpapers/Cityscapes/* $HOME/Dev/wallpapers/Spacescapes/* | sort -R | head -1 | awk '{print $9}' | xargs -I {} feh --bg-center {}
